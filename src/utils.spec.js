52c28e898a178828dedab25c2709b1be1385eef3
Florian Schroedl <hello@florianschroedl.com>
2017-04-23 13:15:32 +0200

    Convert utils spec to jest

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,34 +1,24 @@
-/* eslint-disable no-unused-expressions */
-import chai from 'chai'
-import expectToBeAPromise from 'expect-to-be-a-promise'
-
 import * as util from './utils.js'
 
-chai.use(expectToBeAPromise)
-const { expect } = chai
-
-describe('utils', () => {
-  it('remove http/https from url', () => {
-    expect(util.removeHttpsFromURL('http://my-url.com')).to.equal('my-url.com')
-    expect(util.removeHttpsFromURL('https://my-url.com')).to.equal('my-url.com')
-  })
-
-  it('remove www from url', () => {
-    expect(util.removeWWWFromURL('www.my-url.com')).to.equal('my-url.com')
-  })
-
-  it('replaces special characters with dashes', () => {
-    expect(util.replaceSpecialCharactersWith('my.special.url.com/with/subroutes'))
-      .to.equal('my-special-url-com-with-subroutes')
-  })
-
-  it('sanitizes url', () => {
-    expect(util.sanitizeUrl('https://www.my.special.url.com/with/subroutes'))
-      .to.equal('my-special-url-com-with-subroutes')
-  })
-
-  it('expects promise', () => {
-    expect(util.wait(0)).to.be.a.promise
-  })
-
+it('remove http/https from url', () => {
+  expect(util.removeHttpsFromURL('http://my-url.com')).toBe('my-url.com')
+  expect(util.removeHttpsFromURL('https://my-url.com')).toBe('my-url.com')
 })
+
+it('remove www from url', () => {
+  expect(util.removeWWWFromURL('www.my-url.com')).toBe('my-url.com')
+})
+
+it('replaces special characters with dashes', () => {
+  expect(util.replaceSpecialCharactersWith('my.special.url.com/with/subroutes'))
+    .toBe('my-special-url-com-with-subroutes')
+})
+
+it('sanitizes url', () => {
+  expect(util.sanitizeUrl('https://www.my.special.url.com/with/subroutes'))
+    .toBe('my-special-url-com-with-subroutes')
+})
+
+// it('expects promise', () => {
+//   expect(util.wait(0)).to.be.a.promise
+// })
