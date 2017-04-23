b6906a3c6deb761f278ff9af74c5e62b1644c597
Florian Schroedl <hello@florianschroedl.com>
2017-04-23 16:51:42 +0200

    Add electron mocks

---
+++ src/__mocks__/electron.js
@@ -0,0 +1,8 @@
+export default {
+  require: jest.genMockFunction(),
+  match: jest.genMockFunction(),
+  app: jest.genMockFunction(),
+  BrowserWindow: jest.genMockFunction(),
+  remote: jest.genMockFunction(),
+  dialog: jest.genMockFunction(),
+}
